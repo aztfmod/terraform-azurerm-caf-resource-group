5182026235440ca754058ff43c7a15c22753833d
Laurent <laurent.lesle@microsoft.com>
2019-10-08 14:04:12 +1300

    adding missing base tags

--- module.tf
+++ module.tf
@@ -4,6 +4,6 @@ resource "azurerm_resource_group" "rg" {
 
          name     = "${var.prefix}${each.value.name}"
          location = each.value.location
-         tags     = lookup(each.value, "tags", null)
+         tags     = lookup(each.value, "tags", null) == local.tags ? null : merge(local.tags,each.value.tags)
 
 }
